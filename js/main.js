7db9238dbef31849f2515a822dceef7a18468a31
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-29 16:31:45 +0200

    fix

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// Ricordate di scrivere solo il codice JavaScript e non modificare HTML e CSS
