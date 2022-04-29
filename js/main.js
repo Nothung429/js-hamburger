8253dac7b6aebcac572792b99788077225797b3d
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-29 16:44:35 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 // mostrare / nascondere il menu principale
 // (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
 // Ricordate di scrivere solo il codice JavaScript e non modificare HTML e CSS
+const mobileMenu = document.getElementById("open-menu")
+document.getElementById("open-menu").addEventListener("click",
+    function(){
+        
+    }
+);
