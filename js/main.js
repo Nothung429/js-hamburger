c3ae6513a21492cb8c6abc061e23ef601ff47646
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-29 17:12:57 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 // 1. aprire il burger menu (quando si arriva a risoluzione tablet/mobile)
 const mobileMenuopen = document.getElementById("open-menu")
+const burgerButton = document.getElementById("open-burger")
 mobileMenuopen.addEventListener("click",
     function(){
+        burgerButton.style.display = "block";
         console.log(mobileMenuopen);
-        document.getElementById("open-burger").style.display = "block";
     }
 );
 // 2. chiudere il burger menu
 const mobileMenuclose = document.getElementById("close-menu")
 mobileMenuclose.addEventListener("click",
     function(){
+        burgerButton.style.display = "none";
         console.log(mobileMenuclose);
-        document.getElementById("open-burger").style.display = "none";
     }
 );
